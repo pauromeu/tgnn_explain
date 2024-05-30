2e304af5ce80963f1e7bf89929af0f0054696786
Pau Romeu <romeu.pau@gmail.com>
2024-05-30 10:30:59 +0200

    add data utils to get dataset and loaders

--- src/data.py
+++ src/data.py
@@ -1,4 +1,8 @@
 import torch
+from torch.utils.data import DataLoader
+
+from src.torch_geometric_temporal.pems_bay import PemsBayDatasetLoader
+from src.torch_geometric_temporal.train_test_split import temporal_signal_split
 
 
 class TemporalDataset(torch.utils.data.Dataset):
@@ -21,3 +25,67 @@ def collate_fn(batch):
         torch.stack(edge_weight),
         torch.stack(y),
     )
+
+
+def get_pems_bay_dataset():
+    try:
+        loader = PemsBayDatasetLoader()
+        dataset = loader.get_dataset()
+    except Exception as e:
+        print(f"Error downloading the dataset: {e}")
+        print("Trying to download the dataset without SSL certificate verification")
+        import ssl
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+        loader = PemsBayDatasetLoader()
+        dataset = loader.get_dataset()
+    return dataset
+
+
+def get_loaders(
+    dataset,
+    val_ratio=0.1,
+    test_ratio=0.2,
+    proportion_original_dataset=0.01,
+    batch_size=16,
+    num_workers=1,
+) -> tuple:
+    _, dataset = temporal_signal_split(
+        dataset, train_ratio=1 - proportion_original_dataset
+    )
+    train_dataset_, test_dataset = temporal_signal_split(
+        dataset, train_ratio=1 - test_ratio
+    )
+    train_dataset, val_dataset = temporal_signal_split(
+        train_dataset_, train_ratio=1 - val_ratio
+    )
+
+    print(f"Train dataset length: {train_dataset.snapshot_count}")
+    print(f"Validation dataset length: {val_dataset.snapshot_count}")
+    print(f"Test dataset length: {test_dataset.snapshot_count}")
+
+    train_loader = DataLoader(
+        TemporalDataset(train_dataset),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+    )
+
+    val_loader = DataLoader(
+        TemporalDataset(val_dataset),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+    )
+
+    test_loader = DataLoader(
+        TemporalDataset(test_dataset),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+    )
+
+    return train_loader, val_loader, test_loader
