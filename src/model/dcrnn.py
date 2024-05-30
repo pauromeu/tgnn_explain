14453392a230d0a1c5edc29bcd208866c42d346a
Joobz <tjmjunior@gmail.com>
2024-05-30 02:13:14 +0200

    MOD: DCRNN model accepts batches as input

--- src/model/dcrnn.py
+++ src/model/dcrnn.py
@@ -25,15 +25,22 @@ class DCRNN(nn.Module):
             edge_weight (Tensor): The edge weights [num_edges]
             h (Tensor, optional): The hidden state [num_nodes, out_channels]
         """
-        num_nodes, num_features, P = x.shape
+        
+        if len(x.shape) == 3:
+            x.unsqueeze(0)
+    
+        batch_size, num_nodes, num_features, P = x.shape
+        # num_nodes, num_features, P = x.shape
         out_seq = []
 
+        x = x.permute(3, 1, 0, 2)  # [P, num_nodes, batch_size, num_features]
+
         for t in range(P):
-            h = self.recurrent(x[:, :, t], edge_index, edge_weight, h)
+            h = self.recurrent(x[t, :, :, :], edge_index, edge_weight, h)
             h = F.relu(h)
             out = self.linear(h)
             out_seq.append(out)
 
-        out_seq = torch.stack(out_seq, dim=0)  # [P, num_nodes, num_features]
-        out_seq = out_seq.permute(1, 2, 0)  # [num_nodes, num_features, P]
+        out_seq = torch.stack(out_seq, dim=0)  # [P, num_nodes, batch_size, num_features]
+        out_seq = out_seq.permute(2, 1, 3, 0) # [batch_size, num_nodes, num_features, P]
         return out_seq, h
